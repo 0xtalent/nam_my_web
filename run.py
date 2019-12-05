0544063b7defc37f6aebd97e5e54e13400e89995
marinehayong <marinehayong@gmail.com>
2019-12-05 11:04:46 +0900

    와 진짜 신기하다 플라스크 이용해서 서버 열어서 내용 출력되게까지 했다. 플라스크 좋은데? 장고는 얼마나 더 좋을까 여튼 host를 0.0.0.0으로 설정해서 외부에서 접속가능하게 까지 해봤다. 음 app = Flask 등등 이런거 세부적으로는 모르겠지만, 점차 더 알게되겠지, 일단 열심히 인강 듣는거야! 화이팅!!

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h5>헬로 파이썬</h5>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
